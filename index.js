fccc7ae549c8162a2c5eee963891f35476af7077
ExRafiki <shiizzlle00@gmail.com>
2018-06-15 16:08:58 +0100

    dumb comment out

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect(dbURI);
 
-// app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.use(bodyParser.json());
 app.use('/api', router);
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 app.use(errorHandler);
 
 
