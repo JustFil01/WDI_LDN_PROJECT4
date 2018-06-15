e78df8c3f08467b6e01b49a5f7e1a5f69dc31852
ExRafiki <shiizzlle00@gmail.com>
2018-06-15 15:36:32 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 //------------------------------------------------------------------------------
 import Home from './components/Home';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 //------------------------------------------------------------------------------
 import AuthLogin from './components/auth/Login';
 import AuthRegister from './components/auth/Register';
